e58b561359115427d22632ca6d26c4489badee54
924LeiChen <924LeiChen@users.noreply.github.com>
2017-06-28 08:52:48 +0800

    cn.vo

---
+++ QueryVo.java
@@ -0,0 +1,20 @@
+package cn.vo;
+
+import cn.pojo.Items;
+
+public class QueryVo {
+	//鍟嗗搧瀵硅薄
+	private Items items;
+	//璁㈠崟瀵硅薄...
+	//鐢ㄦ埛瀵硅薄....
+
+	public Items getItems() {
+		return items;
+	}
+
+	public void setItems(Items items) {
+		this.items = items;
+	}
+	
+	
+}
